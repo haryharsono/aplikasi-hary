4943b6932bf16fa4d7861e2fc185e5fb847beee3
hary <311098haryharsono@gmai.com>
2020-06-11 16:01:03 +0800

    konfigurasi pom.xml

---
+++ src/main/java/com/haryharsono/aplikasihary/AplikasiHaryApplication.java
@@ -0,0 +1,13 @@
+package com.haryharsono.aplikasihary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiHaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiHaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haryharsono/aplikasihary/AplikasiHaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haryharsono.aplikasihary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiHaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
